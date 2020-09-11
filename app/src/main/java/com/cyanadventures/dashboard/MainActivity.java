1031dbb3b80beb952d43306655be1cfef4dcf9fa
wdafert <w.dafert@gmail.com>
2020-09-11 14:27:35 +0800

    Initial before google sheets connect

---
+++ app/src/main/java/com/cyanadventures/dashboard/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cyanadventures.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void openQuiz(View view) {
+        Intent intent = new Intent(MainActivity.this,Quiz.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
